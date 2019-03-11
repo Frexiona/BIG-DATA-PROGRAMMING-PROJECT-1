5b8b4c839ff2355fe3c2b79ca5e249482dbfc030
Frexiona <frexiona@foxmail.com>
2019-03-11 16:38:47 +0000

    download books

--- scripts/script_1.sh
+++ scripts/script_1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget -P /home/cs18202514/Project_1/books "http://www.gutenberg.org/files/1524/1524-0.txt"
-wget -P /home/cs18202514/Project_1/books "http://www.gutenberg.org/files/1112/1112.txt"
-wget -P /home/cs18202514/Project_1/books "http://www.gutenberg.org/files/2267/2267.txt"
-wget -P /home/cs18202514/Project_1/books "http://www.gutenberg.org/files/2253/2253.txt"
-wget -P /home/cs18202514/Project_1/books "http://www.gutenberg.org/files/1513/1513-0.txt"
-wget -P /home/cs18202514/Project_1/books "http://www.gutenberg.org/files/1120/1120.txt"
+wget -P /PATH/books "http://www.gutenberg.org/files/1524/1524-0.txt"
+wget -P /PATH/books "http://www.gutenberg.org/files/1112/1112.txt"
+wget -P /PATH/books "http://www.gutenberg.org/files/2267/2267.txt"
+wget -P /PATH/books "http://www.gutenberg.org/files/2253/2253.txt"
+wget -P /PATH/books "http://www.gutenberg.org/files/1513/1513-0.txt"
+wget -P /PATH/books "http://www.gutenberg.org/files/1120/1120.txt"
